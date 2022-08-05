da4bf48d21041deffe2de67ca42d90488a0f71f2
David Waring <djw64@cornell.edu>
2022-08-05 12:42:23 -0400

    Add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import RiLeft from '~icons/ri/arrow-left-line';
+</script>
+
+<template>
+  <div class="py-20">
+    <div class="text-center">
+      <p class="text-sm font-semibold text-primary uppercase tracking-wide dark:text-secondary">404 error</p>
+      <h1 class="text-4xl md:text-5xl lg:text-6xl">Page not found.</h1>
+      <p class="mt-4 lead">Sorry, the page you're looking for does not exist</p>
+      <div class="mt-8">
+        <NuxtLink to="/" class="text-base font-medium text-primary-light hover:text-primary dark:text-secondary-light dark:hover:text-secondary">
+          <RiLeft class="inline" />&nbsp;Go back home
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
