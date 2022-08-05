c1cd05e79bd84037e50929d007d98679a6091b96
David Waring <djw64@cornell.edu>
2022-08-05 14:47:07 -0400

    Refactor Markdown Component use nuxt-runtime-compiler module to render custom vue components

--- components/Directus/Markdown.vue
+++ components/Directus/Markdown.vue
@@ -1,34 +1,41 @@
-<script setup>
+<script>
+  import { DirectusImg } from '#components';
   import MarkdownIt from 'markdown-it';
   import MarkdownItBracketedSpans from 'markdown-it-bracketed-spans';
   import MarkdownItAttrs from 'markdown-it-attrs';
 
-  const props = defineProps({
-    md: {
-      type: String,
-      required: true
+  export default defineNuxtComponent({
+    props: {
+      md: {
+        type: String,
+        required: true
+      }
+    },
+
+    render() {
+
+      // Setup the Markdown Renderer
+      const mdi = new MarkdownIt({
+        html: true,
+        xhtmlOut: true
+      });
+      mdi.use(MarkdownItBracketedSpans);
+      mdi.use(MarkdownItAttrs);
+
+      // Render the markdown into HTML
+      let html = mdi.render(this.md);
+      let re = new RegExp('<img +src=[\'"].*/assets/([a-f0-9\-]+)[\'"](.*) />', 'g');
+      html = html.replace(re, "<DirectusImg hash='\$1' \$2 />");
+
+      // Return the rendered html as the component template
+      // Include any custom components to render
+      return h({
+        template: html,
+        components: {
+          DirectusImg
+        }
+      });
+
     }
   });
-
-  const mdi = new MarkdownIt({
-    html: true,
-    xhtmlOut: true
-  });
-  mdi.use(MarkdownItBracketedSpans);
-  mdi.use(MarkdownItAttrs);
-  
-  /**
-   * Render the markdown as HTML
-   * and replace img tags with DirectusImg tags
-   */
-  const html = computed(() => {
-    let html = mdi.render(props.md);
-    let re = new RegExp('<img +src=[\'"].*/assets/([a-f0-9\-]+)[\'"](.*) />', 'g');
-    html = html.replace(re, "<DirectusImg hash='\$1' \$2 />");
-    return html;
-  });
 </script>
-
-<template>
-  <div class="inline" v-if="md" v-html="html"></div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,24 @@ import { defineNuxtConfig } from 'nuxt';
 import Icons from 'unplugin-icons/vite';
 
 export default defineNuxtConfig({
+
   typescript: {
     strict: true
   },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-directus'],
+
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-directus', 'nuxt-runtime-compiler'],
+
   vite: {
     plugins: [
       Icons({
         autoInstall: true
       })
-    ]
+    ],
   },
+
   directus: {
     url: process.env.DIRECTUS_URL,
     token: process.env.DIRECTUS_TOKEN
   }
+
 });
